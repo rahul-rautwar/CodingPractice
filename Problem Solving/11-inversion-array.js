32fed583ec551e4848e0929fd950f6c1c6d641b3
rahul-rautwar <rahulrautwar@gmail.com>
2023-05-31 23:50:19 +0530

    add inversion function

---
+++ Problem Solving/11-inversion-array.js
@@ -0,0 +1,23 @@
+// Inversion Count for an array indicates – how far (or close) the array is from being sorted. If the array is already sorted, then the inversion count is 0, but if the array is sorted in reverse order, the inversion count is the maximum.
+
+// Given an array arr[]. The task is to find the inversion count of arr[]. Where two elements arr[i] and arr[j] form an inversion if a[i] > a[j] and i < j.
+
+// Examples:
+
+// Input: arr[] = {8, 4, 2, 1}
+// Output: 6
+// Explanation: Given array has six inversions: (8, 4), (4, 2), (8, 2), (8, 1), (4, 1), (2, 1).
+
+function inversion(arr) {
+  let count = 0;
+  for (var i = 0; i < arr.length; i++) {
+    for (var j = i + 1; j < arr.length; j++) {
+      if (arr[i] > arr[j]) {
+        count++;
+      }
+    }
+  }
+  return count;
+}
+console.log("inversion array", inversion([8, 4, 2, 1]));
+console.log("inversion array", inversion([1, 20, 6, 4, 5]));
